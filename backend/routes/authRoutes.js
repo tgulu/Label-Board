3e48e501d4d3dd173cbef5f926d1c52c0d55ddd3
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-26 18:52:59 +0100

    add routes for pages

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+//Auth Routes
+router.post("/login", loginUser); // Login route
+router.post("/register", registerUser); // Register route
+router.get("/profile", protect, getUserProfile); // Get User Profile
+router.get("profile", protect, updateUserProfile); // Update Profile
+
+module.exports = router;
