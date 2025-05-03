678d5b4ba868fda5c0d320f901c7f33ce71305ee
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-03 12:45:31 +0100

    add routes for creating users and passwords

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,23 +10,84 @@ const generateToken = (userId) => {
 // @desc    Register a new user
 // @route   POST /api/auth/register
 // @access  Public
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl, adminInviteToken } =
+      req.body;
+
+    //check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    let role = "memeber";
+    if (
+      adminInviteToken &&
+      adminInviteToken === process.env.ADMIN_INVITE_TOKEN
+    ) {
+      role = "admin";
+    }
+
+    // Hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+      role,
+    });
+
+    //Return user data with JWT
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 // @desc Login user
 // @route POST /api/auth/login
 // @access Public
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 // @desc Get user profile
 // @route GET /api/auth/profile
 // @access Private (Requires JWT)
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 // @desc Update user profile
 // @route PUT /api/auth/profile
 // @access Private (Requires JWT)
 
-const updateUserProfile = async (req, res) => {};
+const updateUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 module.exports = {
   registerUser,
