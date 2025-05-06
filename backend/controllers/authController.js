52a6883a50712a6341268ba15b9d7a616faf2220
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-06 18:42:52 +0100

    add error handler for get user profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -97,6 +97,11 @@ const loginUser = async (req, res) => {
 // @access Private (Requires JWT)
 const getUserProfile = async (req, res) => {
   try {
+    const user = await User.findById(req.user._id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
