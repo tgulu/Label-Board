5a5c6ed245dafd1b8b3c4530815d118b0fcdd20a
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-07 14:10:03 +0100

    add config for img upload and img type

---
+++ backend/middlewares/uploadMiddleware.js
@@ -0,0 +1,24 @@
+const mutler = require("multer");
+
+// Configure storage for uploaded files
+const storage = mutler.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Directory to save uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`); // Unique filename
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpeg, .jpg and .png formats are allowed"), false);
+  }
+
+  const upload = multer({ storage, fileFilter });
+};
+
+module.exports = upload;
