eab87a21216808fdc3222eeba774b24e8842b834
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-07 14:00:42 +0100

    add routing for uploading img

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -16,4 +16,14 @@ router.post("/register", registerUser); // Register route
 router.get("/profile", protect, getUserProfile); // Get User Profile
 router.put("/profile", protect, updateUserProfile); // Update Profile
 
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
+
 module.exports = router;
