1c8aeeecaae08f8acf5dd674c40bef0b88a5183d
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-07 14:35:27 +0100

    fix typo in user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { adminOnly } = require("../middlewares/authMiddleware");
+const { adminOnly, protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 // User Management Routes
-router.get("/", proectect, adminOnly, getAllUsers); // Get all users (admin only)
+router.get("/", protect, adminOnly, getUsers); // Get all users (admin only)
 router.get("/:id", protect, adminOnly, getUserById); // Get user by ID (admin only)
 router.delete("/:id", protect, adminOnly, deleteUser); // Delete user by ID (admin only)
 
