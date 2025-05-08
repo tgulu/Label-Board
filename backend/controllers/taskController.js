81a5bb7870354db0a7076adc8a1f5affe524fb9f
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-08 14:13:28 +0100

    create logic for updateTask api

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -135,7 +135,27 @@ const createTask = async (req, res) => {
 // @access  Private
 const updateTask = async (req, res) => {
   try {
-    // logic here
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    task.title = req.body.title || task.title;
+    task.description = req.body.description || task.description;
+    task.priority = req.body.priority || task.priority;
+    task.dueDate = req.body.dueDate || task.dueDate;
+    task.todoChecklist = req.body.todoChecklist || task.todoChecklist;
+    task.attachments = req.body.attachments || task.attachments;
+
+    if (req.body.assignedTo) {
+      if (!Array.isArray(req.body.assignedTo)) {
+        return res
+          .status(400)
+          .json({ message: "assignedTo must be an array of user IDs" });
+      }
+      task.assignedTo = req.body.assignedTo;
+    }
+    const updatedTask = await task.save();
+    res.json({ message: "Task updated successfully", task: updatedTask });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
