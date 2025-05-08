1cc5b70225412f544969bbd8be05fd7df7cb1d5d
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-08 15:08:08 +0100

    add logic to updateTaskStatus function

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -182,7 +182,28 @@ const deleteTask = async (req, res) => {
 // @access  Private
 const updateTaskStatus = async (req, res) => {
   try {
-    // logic here
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    const isAssigned = task.assignedTo.some(
+      (userId) => userId.toString() === req.user._id.toString()
+    );
+
+    if (!isAssigned && req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to update this task" });
+    }
+
+    task.status = req.body.status || task.status;
+
+    if (task.status === "Completed") {
+      task.todoChecklist.forEach((item) => (item.completed = true));
+      task.progress = 100;
+    }
+
+    await task.save();
+    res.json({ message: "Task status updated successfully", task });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
