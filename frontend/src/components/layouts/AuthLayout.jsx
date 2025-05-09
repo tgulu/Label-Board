e8b200196d8e2f2e4cb7b53a861d43f68cd8b8ba
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-09 20:51:51 +0100

    add css for background imgs

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import UIimage from "../../assets/images/taskManager.png";
 
-const AuthLayout = () => {
-  return <div></div>;
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex">
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-lg font-medium text-black">Label Board Manager</h2>
+        {children}
+      </div>
+      {/* 1:38:36 */}
+      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-[url('/src/assets/images/blue.jpg')]   bg-cover bg-no-repeat bg-center overflow-hidden p-8">
+        <img src={UIimage} alt="" className="w-64 lg:w-[90%]" />
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
