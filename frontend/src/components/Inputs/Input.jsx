ec5fd9699253237973f791d96a2651e5441ae7ab
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-10 15:52:46 +0100

    add password check for form

--- frontend/src/components/Inputs/Input.jsx
+++ frontend/src/components/Inputs/Input.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRehEye, FaRegEyeSlash } from "react-icons/fa";
 
-const Input = ({ value, onChange, label, placeholder }) => {
-  return <div></div>;
+const Input = ({ value, onChange, label, placeholder, type }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      <label className="text-[13px] text-slate-800">{label}</label>
+
+      <div className="input-box">
+        <input
+          type={
+            type == "password" ? (showPassword ? "text" : "password") : type
+          }
+          placeholder={placeholder}
+          className="w-full bg-transparent outline-none"
+          value={value}
+          onChange={(e) => onChange(e)}
+        />
+
+        {type === "password" && (
+          <>
+            {showPassword ? (
+              <FaRehEye
+                className="text-primary cursor-pointer"
+                size={22}
+                onClick={() => toggleShowPassword()}
+              />
+            ) : (
+              <FaRegEyeSlash
+                size={22}
+                className="text-slate-400 cursor-pointer"
+                onClick={() => toggleShowPassword()}
+              />
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Input;
