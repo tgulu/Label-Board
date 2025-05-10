c177e67851292b63014ee476c8f9547534c9492c
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-10 17:20:47 +0100

    add email validation regex

---
+++ frontend/src/utils/helper.js
@@ -0,0 +1,8 @@
+export const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
+export const validatePassword = (password) => {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  return regex.test(password);
+};
