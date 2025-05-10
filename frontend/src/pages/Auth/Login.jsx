37eea5fe5e38ca186243ab18ac68f0d6b1cb5423
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-10 15:31:02 +0100

    add form for email inputs

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <AuthLayout>Login</AuthLayout>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const naviagete = useNavigate();
+
+  // Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-600 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={(target) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="stevie@wonder.com"
+            type="text"
+          ></Input>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default Login;
