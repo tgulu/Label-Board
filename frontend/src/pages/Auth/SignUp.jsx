a4590c9fea232326708fd31a1f04179f57f41f23
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-10 18:54:36 +0100

    add main set-up for page

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [adminInviteToken, setAdminInviteToken] = useState("");
+
+  const [error, setError] = useState(null);
+
+  // Handle Sign Form Submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!fullName) {
+      setError("Please enter full name");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter your password");
+      return;
+    }
+
+    setError("");
+
+    //Sign up API
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-600 mt-[5px] mb-6">
+          Sign up to create an account and start managing your tracks and
+          releases.
+        </p>
+
+        <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default SignUp;
