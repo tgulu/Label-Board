954d4d92e747f3f8a5b711c871688dc6ca610d72
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-12 14:12:08 +0100

    add file for side menu for pages

---
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useContext, useState } from "react";
+import { SIDE_MENU_DATA, SIDE_MENU_USER_DATA } from "../../utils/data";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
+
+const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
+  const [sideMenuData, setSideMenuData] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleClick = (route) => {
+    if (route === "logout") {
+      handleLogout();
+      return;
+    }
+
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    if (user) {
+      setSideMenuData(
+        user?.role === "admin" ? SIDE_MENU_DATA : SIDE_MENU_USER_DATA
+      );
+    }
+    return () => {};
+  }, [user]);
+
+  return <div>SIDEMENU</div>;
+};
+
+export default SideMenu;
