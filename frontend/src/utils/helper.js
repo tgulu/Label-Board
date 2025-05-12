bdb47990d318e20577aae1f805f7bcca486360bd
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-12 21:00:43 +0100

    create function addthousandSeparator to format a number

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -6,3 +6,14 @@ export const validatePassword = (password) => {
   const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
   return regex.test(password);
 };
+
+export const addthousandSeparator = (num) => {
+  if (num == null || isNaN(num)) return "";
+
+  const [integerPart, fractionPart] = num.toString().split(".");
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return fractionPart
+    ? `${formattedInteger}.${fractionPart}`
+    : formattedInteger;
+};
