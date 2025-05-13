7694296f575c6d094aa02b8d1102a9fd2bb44c61
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-13 14:54:52 +0100

    create UI for creating tasks

--- frontend/src/pages/Admin/CreateTask.jsx
+++ frontend/src/pages/Admin/CreateTask.jsx
@@ -1,7 +1,255 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { PRIORITY_DATA } from "../../utils/data";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import toast from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import moment from "moment";
+import { LuTrash2 } from "react-icons/lu";
+import SelectDropdown from "..//../components/Inputs/SelectDropDown";
+import DeleteAlert from "../../components/DeleteAlert";
+import Modal from "../../components/Modal";
 
 const CreateTask = () => {
-  return <div></div>;
+  const location = useLocation();
+  const { taskId } = location.state || {};
+  const navigate = useNavigate();
+
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "",
+    priority: "Low",
+    dueDate: null,
+  });
+
+  const [currentTask, setCurrentTask] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
+
+  const handleValueChange = (key, value) => {
+    setTaskData((prevData) => ({ ...prevData, [key]: value }));
+  };
+
+  const clearData = () => {
+    setTaskData({
+      title: "",
+      description: "",
+      priority: "Low",
+      dueDate: null,
+    });
+  };
+
+  const createTask = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.post(API_PATHS.TASKS.CREATE_TASK, {
+        ...taskData,
+        dueDate: new Date(taskData.dueDate).toISOString(),
+      });
+
+      toast.success("Task Created Successfully");
+      clearData();
+    } catch (error) {
+      console.error("Error creating task:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateTask = async () => {
+    setLoading(true);
+
+    try {
+      await axiosInstance.put(API_PATHS.TASKS.UPDATE_TASK(taskId), {
+        ...taskData,
+        dueDate: new Date(taskData.dueDate).toISOString(),
+      });
+
+      toast.success("Task Updated Successfully");
+    } catch (error) {
+      console.error("Error updating task:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = async () => {
+    setError(null);
+
+    if (!taskData.title.trim()) {
+      setError("Title is required.");
+      return;
+    }
+    if (!taskData.description.trim()) {
+      setError("Description is required.");
+      return;
+    }
+    if (!taskData.dueDate) {
+      setError("Due date is required.");
+      return;
+    }
+
+    if (taskId) {
+      updateTask();
+    } else {
+      createTask();
+    }
+  };
+
+  const getTaskDetailsByID = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_TASK_BY_ID(taskId)
+      );
+
+      if (response.data) {
+        const taskInfo = response.data;
+        setCurrentTask(taskInfo);
+
+        setTaskData({
+          title: taskInfo.title,
+          description: taskInfo.description,
+          priority: taskInfo.priority,
+          dueDate: taskInfo.dueDate
+            ? moment(taskInfo.dueDate).format("YYYY-MM-DD")
+            : null,
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching task:", error);
+    }
+  };
+
+  // Delete Task
+  const deleteTask = async () => {
+    try {
+      await axiosInstance.delete(API_PATHS.TASKS.DELETE_TASK(taskId));
+      setOpenDeleteAlert(false);
+      toast.success("Task deleted successfully");
+      navigate("/admin/tasks");
+    } catch (error) {
+      console.error(
+        "Error deleting task:",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (taskId) {
+      getTaskDetailsByID();
+    }
+  }, [taskId]);
+
+  return (
+    <DashboardLayout activeMenu="Create Task">
+      <div className="mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="form-card col-span-3">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl md:text-xl font-medium">
+                {taskId ? "Update Task" : "Create Task"}
+              </h2>
+
+              {taskId && (
+                <button
+                  className="flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-rose-50 rounded px-2 py-1 border border-rose-100 hover:border-rose-300 cursor-pointer"
+                  onClick={() => setOpenDeleteAlert(true)}
+                >
+                  <LuTrash2 className="text-base" /> Delete
+                </button>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label className="text-xs font-medium text-slate-600">
+                Task Title
+              </label>
+              <input
+                placeholder="New Track Mix"
+                className="form-input"
+                value={taskData.title}
+                onChange={({ target }) =>
+                  handleValueChange("title", target.value)
+                }
+              />
+            </div>
+
+            <div className="mt-3">
+              <label className="text-xs font-medium text-slate-600">
+                Description
+              </label>
+              <textarea
+                placeholder="Describe task"
+                className="form-input"
+                rows={4}
+                value={taskData.description}
+                onChange={({ target }) =>
+                  handleValueChange("description", target.value)
+                }
+              />
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6 md:col-span-4">
+                <label className="text-xs font-medium text-slate-600">
+                  Priority
+                </label>
+                <SelectDropdown
+                  options={PRIORITY_DATA}
+                  value={taskData.priority}
+                  onChange={(value) => handleValueChange("priority", value)}
+                  placeholder="Select Priority"
+                />
+              </div>
+
+              <div className="col-span-6 md:col-span-4">
+                <label className="text-xs font-medium text-slate-600">
+                  Due Date
+                </label>
+                <input
+                  type="date"
+                  className="form-input"
+                  value={taskData.dueDate}
+                  onChange={({ target }) =>
+                    handleValueChange("dueDate", target.value)
+                  }
+                />
+              </div>
+            </div>
+
+            {error && (
+              <p className="text-xs font-medium text-red-500 mt-5">{error}</p>
+            )}
+
+            <div className="flex justify-end mt-7">
+              <button
+                className="add-btn"
+                onClick={handleSubmit}
+                disabled={loading}
+              >
+                {taskId ? "UPDATE TASK" : "CREATE TASK"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Modal
+        isOpen={openDeleteAlert}
+        onClose={() => setOpenDeleteAlert(false)}
+        title="Delete Task"
+      >
+        <DeleteAlert
+          content="Are you sure you want to delete this task?"
+          onDelete={deleteTask}
+        />
+      </Modal>
+    </DashboardLayout>
+  );
 };
 
 export default CreateTask;
