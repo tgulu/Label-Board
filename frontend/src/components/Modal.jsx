53570acb77c6a86ab6caf1f14508580374058d01
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-13 16:25:56 +0100

    inverse color for pop up modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -4,18 +4,16 @@ const Modal = ({ children, isOpen, onClose, title }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50">
+    <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-white/30 backdrop-blur-sm">
       <div className="relative p-4 w-full max-w-2xl max-h-full">
         {/* Modal content */}
-        <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+        <div className="relative bg-gray-50 rounded-lg shadow-sm border border-gray-200">
           {/* Modal header */}
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-            <h3 className="text-lg font-medium text-gray-900 dark:text-white">
-              {title}
-            </h3>
+          <div className="flex items-center justify-between p-4 md:p-5 border-b border-gray-200 rounded-t">
+            <h3 className="text-lg font-medium text-gray-900">{title}</h3>
             <button
               type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+              className="text-gray-500 bg-transparent hover:bg-gray-100 hover:text-gray-800 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center cursor-pointer"
               onClick={onClose}
             >
               <svg
