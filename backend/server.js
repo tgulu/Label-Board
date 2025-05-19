0e20db83f325cde92ed351b97a921316b2e71384
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-05-19 15:53:37 +0100

    comment out prrocess.env.node

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,19 @@ app.use("/api/tasks", taskRoutes);
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// const __dirname = path.resolve();
+
 //Start Server
 const PORT = process.env.PORT || 5000;
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+//     );
+//   });
+// }
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
